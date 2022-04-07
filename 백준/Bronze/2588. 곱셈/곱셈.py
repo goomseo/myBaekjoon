f6b8ef61fdc4e0e3c00bbebcc409230450cef0dd
goomseo <102874992+goomseo@users.noreply.github.com>
2022-04-07 20:12:10 +0900

    [Bronze IV] Title: 곱셈, Time: 68 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,12 @@
+i = int(input())
+j = input()
+
+first = i * int(j[2])
+second = i * int(j[1])
+third = i * int(j[0])
+result = i * int(j)
+
+print(first)
+print(second)
+print(third)
+print(result)
