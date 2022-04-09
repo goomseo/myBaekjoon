38e93fd7771f8c2cdcef4ec882ca26c12bc84bec
goomseo <102874992+goomseo@users.noreply.github.com>
2022-04-09 23:17:41 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1276 ms, Memory: 75672 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,9 +1,10 @@
 import sys
 input = sys.stdin.readline
+print = sys.stdout.write
 
 qtyNum = int(input())
 numList = [int(input()) for _ in range(qtyNum)]
 numList.sort()
 
 for i in range(qtyNum):
-    print(numList[i])
+    print(str(numList[i]) + '\n')
