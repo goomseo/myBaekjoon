2737114154bcb4c4228be9da2959e3ce3c2f9247
goomseo <102874992+goomseo@users.noreply.github.com>
2022-04-10 23:36:49 +0900

    [Bronze I] Title: 평균, Time: 176 ms, Memory: 45520 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -5,5 +5,5 @@ input = sys.stdin.readline
 qtySubject = int(input())
 scoreList = list(map(int, input().split()))
 
-newScoreList = [scoreList[i] / max(scoreList) * 100 for i in range(qtySubject)]
-print(statistics.mean(newScoreList))
+scoreList = [scoreList[i] / max(scoreList) * 100 for i in range(qtySubject)]
+print(statistics.mean(scoreList))
