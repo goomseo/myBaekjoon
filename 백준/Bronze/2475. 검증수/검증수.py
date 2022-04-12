6282457d3a1c675b814610d9942f5c827e42bf6c
goomseo <102874992+goomseo@users.noreply.github.com>
2022-04-12 15:48:49 +0900

    [Bronze V] Title: 검증수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 numList = list(map(int, input().split()))
 
 total = 0
-for i in range(len(numList)):
+for i in range(5):
     total += numList[i] ** 2
 
 print(total % 10)
