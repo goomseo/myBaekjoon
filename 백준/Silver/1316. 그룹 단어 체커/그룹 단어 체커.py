2b684be321872df57a8754482a2e9f1568ec42fb
goomseo <102874992+goomseo@users.noreply.github.com>
2022-05-07 00:16:22 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,34 +1,36 @@
 import sys
 input = sys.stdin.readline
 
-def findGroupWord(word):
+def findGW(word):
     wordList = list(word)
-    charSet = list(set(word))
+    curWord = ''
+    wordSet = set()
+    for w in wordList:
+        if w in wordSet:
+            if curWord == w :
+                continue
+            # elif w in wordSet:#새로운 문자인데 등록된 경우
+            else:
+                return False
+        #         wordSet.add(w)
+        #         curWord = w
+        else :
+            wordSet.add(w)
+            curWord = w
 
-    idxGaps = []
-    for char in charSet:
-        idx1 = wordList.index(char)
-        reversedWordList = wordList[::-1]
-        idx2 = len(word) - (reversedWordList.index(char) + 1)
-        idxGaps.append(idx2 - idx1 + 1)
-
-    count = 0
-    for i in range(len(charSet)):
-        if idxGaps[i] == wordList.count(charSet[i]):
-            count += 1
-
-    if count == len(charSet):
-        return True
-    else:
-        return False
+    return True
 
 def main():
+    # 입력받을 단어의 개수 입력
+    # 입력받은 개수만큼 단어 입력
     qtyWords = int(input().rstrip())
     words = [input().rstrip() for _ in range(qtyWords)]
     qtyGroupWords = 0
 
+    # 한 단어씩 검사
     for word in words:
-        if findGroupWord(word) == True:
+        # if findGroupWord(word) == True:
+        if findGW(word) == True:
             qtyGroupWords += 1
     
     print(qtyGroupWords)
