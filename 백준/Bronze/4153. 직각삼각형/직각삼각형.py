6a9e8bad59e4463ad799d3cfeb14ec1cd08f6f54
goomseo <102874992+goomseo@users.noreply.github.com>
2022-06-10 00:33:12 +0900

    [Bronze III] Title: 직각삼각형, Time: 104 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -6,14 +6,10 @@ def input():
 while True:
     nums = list(map(int, input().split()))
 
-    if (nums[0] == 0) and (nums[1] == 0) and (nums[2] == 0):
+    if not sum(nums):
         break
 
-    for hypotenuse in nums:
-        tmp = nums.copy()
-        tmp.remove(hypotenuse)
-        if hypotenuse ** 2 == tmp[0] ** 2 + tmp[1] ** 2:
-            print('right')
-            break
-        if nums.index(hypotenuse) == 2:
-            print('wrong')
+    hypotenuse = max(nums)
+    nums.remove(hypotenuse)
+
+    print('right' if (hypotenuse ** 2) == ((nums[0] ** 2) + (nums[1] ** 2)) else 'wrong')
