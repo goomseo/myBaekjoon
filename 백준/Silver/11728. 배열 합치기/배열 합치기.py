26202f8f534cffb0f8a12775bd32aa9c44df7d65
goomseo <102874992+goomseo@users.noreply.github.com>
2022-06-20 22:38:24 +0900

    [Silver V] Title: 배열 합치기, Time: 916 ms, Memory: 358248 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -5,4 +5,4 @@ input()
 arr1 = list(map(int, input().rstrip().split()))
 arr2 = list(map(int, input().rstrip().split()))
 
-[print(num, end = ' ') for num in sorted(arr1 + arr2)]
+print(' '.join(map(str, sorted(arr1 + arr2))))
