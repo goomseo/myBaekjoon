6972e971cbd02adf29b81f4c45830d5295438b2e
goomseo <102874992+goomseo@users.noreply.github.com>
2022-06-24 18:46:07 +0900

    [Bronze V] Title: 아!, Time: 112 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Bronze/4999. 아！/아！.py
+++ 백준/Bronze/4999. 아！/아！.py
@@ -1,6 +1,1 @@
-import sys
-input = sys.stdin.readline
-
-patient, doctor = [len(input().rstrip()) for _ in range(2)]
-
-print('go' if patient >= doctor else 'no')
+print('go' if len(input()) >= len(input()) else 'no')
