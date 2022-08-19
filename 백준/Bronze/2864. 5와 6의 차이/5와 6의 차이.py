387ad7ff8f0f3b5d3ea6ace2b48b40d6d3ae0692
goomseo <102874992+goomseo@users.noreply.github.com>
2022-08-19 23:29:49 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 100 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.py
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.py
@@ -1,12 +1,9 @@
 import sys
 input = sys.stdin.readline
 
-def getMin(num):
-    return int(num.replace('6', '5'))
-
-def getMax(num):
-    return int(num.replace('5', '6'))
-
 a, b = input().rstrip().split()
+
+getMin = lambda num: int(num.replace('6', '5'))
+getMax = lambda num: int(num.replace('5', '6'))
 
 print(getMin(a) + getMin(b), getMax(a) + getMax(b))
