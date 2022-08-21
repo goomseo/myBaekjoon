7aa4e338ff0110f39fa490f86f14b33a246abee1
goomseo <102874992+goomseo@users.noreply.github.com>
2022-08-21 22:46:17 +0900

    [Bronze II] Title: 대표값, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2592. 대표값/대표값.py
+++ 백준/Bronze/2592. 대표값/대표값.py
@@ -1,8 +1,7 @@
-import statistics
 import sys
 input = sys.stdin.readline
 
 nums = [int(input()) for _ in range(10)]
 
-print(statistics.mean(nums))
-print(statistics.mode(nums))
+print(sum(nums) // len(nums))
+print(max(nums, key = lambda i: nums.count(i)))
