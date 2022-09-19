290b12acae00ad3da25456e072c7520304fa2171
goomseo <102874992+goomseo@users.noreply.github.com>
2022-09-19 18:00:48 +0900

    [Bronze II] Title: 오타맨 고창영, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2711. 오타맨 고창영/오타맨 고창영.py
+++ 백준/Bronze/2711. 오타맨 고창영/오타맨 고창영.py
@@ -3,5 +3,4 @@ input = sys.stdin.readline
 
 for _ in range(int(input())):
     idx, word = input().rstrip().split()
-    word = word[:(int(idx)-1)] + word[int(idx):]
-    print(word)
+    print(word[:(int(idx)-1)] + word[int(idx):])
