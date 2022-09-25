99399559df828f56d77141b93d960c106e0067a2
goomseo <102874992+goomseo@users.noreply.github.com>
2022-09-25 23:56:44 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,7 +1,12 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 
-for i in range(1, (2 * N), 2):
-    print(' ' * ((2 * N - i) // 2) + '*' * i)
+for i in range((N - 1), 0, -1):
+    print(' ' * i + '*' * (2 * (N - i) - 1))
 
-for i in range((2 * N - 3), 0, -2):
-    print(' ' * ((2 * N - i) // 2) + '*' * i)
+print('*' * (2 * N - 1))
+
+for j in range(1, N):
+    print(' ' * j + '*' * (2 * (N - j) - 1)) 
