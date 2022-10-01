86ec88f5c1cac6db31ac3ca137aff403a782d0b3
goomseo <102874992+goomseo@users.noreply.github.com>
2022-10-01 15:01:14 +0900

    [Bronze III] Title: 주사위 게임, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/2476. 주사위 게임/주사위 게임.py
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -1,31 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-prize = []
-for _ in range(int(input())):
-    dice_map = map(int, input().rstrip().split())
-    
-    diceCount_1 = dict()
-    for dice in dice_map:
-        if not(dice in diceCount_1):
-            diceCount_1[dice] = 1
-        else:
-            diceCount_1[dice] += 1
+def getPrize(dice_list):
+    dice_set = set(dice_list)
 
-    diceCount_2 = {3: [], 2: [], 1: []}
-    for dice in diceCount_1.keys():
-        if diceCount_1[dice] == 3:
-            diceCount_2[3].append(dice)
-        elif diceCount_1[dice] == 2:
-            diceCount_2[2].append(dice)
-        else:
-            diceCount_2[1].append(dice)
+    if len(dice_set) == 1:
+        return 1000 * (10 + dice_list[0])
+    elif len(dice_set) == 2:
+        for dice in dice_set:
+            if dice_list.count(dice) == 2:
+                same_dice = dice
+                break
 
-    if len(diceCount_2[3]):
-        prize.append(1000 * (10 + diceCount_2[3][0]))
-    elif len(diceCount_2[2]):
-        prize.append(100 * (10 + diceCount_2[2][0]))
+        return 100 * (10 + same_dice)
     else:
-        prize.append(max(diceCount_2[1]) * 100)
+        return 100 * max(dice_list)
 
-print(max(prize))
+def main():
+    prize_list = []
+    [prize_list.append(getPrize(list(map(int, input().rstrip().split())))) for _ in range(int(input()))]
+    
+    print(max(prize_list))
+
+main()
