7d58eeb3d0f24c9955282198fee1d425cca17d70
goomseo <102874992+goomseo@users.noreply.github.com>
2023-02-04 16:05:28 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.c
+++ 백준/Bronze/10869. 사칙연산/사칙연산.c
@@ -5,11 +5,7 @@ int main(void)
     int a, b;
     scanf("%d %d", &a, &b);
 
-    printf("%d\n", a + b);
-    printf("%d\n", a - b);
-    printf("%d\n", a * b);
-    printf("%d\n", a / b);
-    printf("%d\n", a % b);
+    printf("%d\n%d\n%d\n%d\n%d\n", a + b, a - b, a * b, a / b, a % b);
 
     return 0;
 }
