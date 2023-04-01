1bf5a1c879b2a21f6ba4480aed2f65f576545acb
goomseo <102874992+goomseo@users.noreply.github.com>
2023-04-01 22:26:15 +0900

    [Unrated] Title: 내 학점을 구해줘, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/10984. 내 학점을 구해줘/내 학점을 구해줘.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+
+
+using namespace std;
+
+class Grades{
+private:
+    int courses;
+    int credits[11] = {0};
+    double grades[11] = {0};
+public:
+    Grades(int courses) {this -> courses = courses;}
+    void read();
+    int getCredits();
+    double getGpa();
+};
+
+void Grades::read() {
+    for (int i = 0; i < courses; i++)
+        cin >> credits[i] >> grades[i];
+}
+
+int Grades::getCredits() {
+    int sum = 0;
+    for (int i = 0; i < courses; i++)
+        sum += credits[i];
+
+    return sum;
+}
+
+double Grades::getGpa() {
+    double tmp = 0;
+    for (int i = 0; i < courses; i++)
+        tmp += credits[i] * grades[i];
+
+    return tmp / (this -> getCredits());
+}
+
+int main(){
+    int semesters;
+    cin >> semesters;
+
+    int courses;
+    for (int i = 0; i < semesters; i++){
+        cin >> courses;
+        Grades grade(courses);
+
+        grade.read();
+
+        cout << grade.getCredits() << ' ';
+
+        cout << fixed;
+        cout.precision(1);
+        cout << grade.getGpa() << endl;
+    }
+}
