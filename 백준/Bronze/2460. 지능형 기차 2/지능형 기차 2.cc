d9e0edbe79777cb34e67ddc2391eab6a79996c9f
Minseo Gu <goomseo1743@gmail.com>
2023-07-08 22:18:57 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int max = 0;
+    int passengers = 0;
+    int out, in;
+    for (int i = 0; i < 10; ++i) {
+        cin >> out >> in;
+        passengers -= out;
+        if (passengers > max)
+            max = passengers;
+        passengers += in;
+        if (passengers > max)
+            max = passengers;
+    }
+
+    cout << max;
+}
