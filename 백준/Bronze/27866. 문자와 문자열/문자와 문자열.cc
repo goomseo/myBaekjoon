1104853ad486b4bb6b8766d8dbc33fc3631cba3b
Minseo Gu <goomseo1743@gmail.com>
2023-07-10 19:27:03 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    string s;
+    int i;
+    cin >> s;
+    cin >> i;
+
+    cout << s[i - 1];
+}
