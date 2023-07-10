5d43d681f05537db3a35e61a523d07c7872e6d32
Minseo Gu <goomseo1743@gmail.com>
2023-07-10 20:28:15 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,32 +1,29 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int cardNum[101];
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
 
-	int qtrCard, goal;
-	cin >> qtrCard >> goal;
+    int n, m;
+    cin >> n >> m;
 
-	for (int i = 0; i < qtrCard; i++) {
-		cin >> cardNum[i];
-	}
+    int arr[n];
+    for (int i = 0; i < n; ++i)
+        cin >> arr[i];
 
-	int sumResult = 0;
-	for (int j = 0; j < qtrCard - 2; j++) {
-		for (int k = j + 1; k < qtrCard - 1; k++) {
-			for (int l = k + 1; l < qtrCard; l++) {
-				if (sumResult <= cardNum[j] + cardNum[k] + cardNum[l] && cardNum[j] + cardNum[k] + cardNum[l] <= goal) {
-					sumResult = cardNum[j] + cardNum[k] + cardNum[l];
-				}
-			}
-		}
-	}
+    int max = 0, sum;
+    for (int i = 0; i < n - 2; ++i) {
+        for (int j = i + 1; j < n - 1; ++j) {
+            for (int k = j + 1; k < n; ++k) {
+                sum = arr[i] + arr[j] + arr[k];
+                if (sum <= m and sum >= max)
+                    max = sum;
+            }
+        }
+    }
 
-	cout << sumResult;
-
-	return 0;
+    cout << max;
 }
