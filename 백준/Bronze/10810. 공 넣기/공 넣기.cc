e055bf735e1d5a26cca1f094533ac7ea9da310f3
Minseo Gu <goomseo1743@gmail.com>
2023-07-20 14:38:01 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> vec(n, 0);
+    int start, end, num;
+    for (int i = 0; i < m; ++i) {
+        cin >> start >> end >> num;
+        for (int j = start; j < end + 1; ++j) {
+            vec[j - 1] = num;
+        }
+    }
+
+    for (int i : vec)
+        cout << i << ' ';
+}
