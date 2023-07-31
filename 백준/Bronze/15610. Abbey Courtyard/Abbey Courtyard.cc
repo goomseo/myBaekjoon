999e164cd81572d72f283dc09c346e57aaed63d4
Minseo Gu <goomseo1743@gmail.com>
2023-07-31 23:50:23 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double a;
+    cin >> a;
+
+    cout << fixed;
+    cout.precision(6);
+    cout << sqrt(a) * 4;
+}
