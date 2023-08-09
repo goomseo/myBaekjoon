39261f0aa414ccecdc707ee2f3b75280f97c0ad6
Minseo Gu <goomseo1743@gmail.com>
2023-08-09 19:06:16 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.cc
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.cc
@@ -1,12 +1,6 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <numeric>
-#include <cmath>
 
 using namespace std;
-typedef unsigned long long ull;
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -20,23 +14,19 @@ int main() {
         return 0;
     }
 
-    for (int i = 0; i < n - 1; ++i) {
+    for (int i = 0; i < n - 1; ++i)
         cout << " ";
-    }
     cout << "*\n";
 
     for (int i = 0; i < n - 2; ++i) {
-        for (int j = n - 2 - i; j > 0; --j) {
+        for (int j = n - 2 - i; j > 0; --j)
             cout << " ";
-        }
         cout << "*";
-        for (int j = 0; j < 2 * i + 1; ++j) {
+        for (int j = 0; j < 2 * i + 1; ++j)
             cout << " ";
-        }
         cout << "*\n";
     }
 
-    for (int i = 0; i < 2 * n - 1; ++i) {
+    for (int i = 0; i < 2 * n - 1; ++i)
         cout << "*";
-    }
 }
