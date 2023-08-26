5d7acca41c0d12d28815723d24b764bc4a530043
Minseo Gu <goomseo1743@gmail.com>
2023-08-26 21:35:50 +0900

    [Bronze IV] Title: 공백 없는 A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15873. 공백 없는 A＋B/공백 없는 A＋B.cc
+++ 백준/Bronze/15873. 공백 없는 A＋B/공백 없는 A＋B.cc
@@ -5,7 +5,5 @@ using namespace std;
 int main() {
     int input;
     cin >> input;
-
-    int answer = (input % 10 == 0) ? input / 100 + input % 100 : input / 10 + input % 10;
-    cout << answer;
+    cout << ((input % 10 == 0) ? input / 100 + input % 100 : input / 10 + input % 10);
 }
